8bc61a20d3a53c23936cf8ee9d7ea35e6b8c2e60
BravoBot21 <bravobot21@gmail.com>
2025-03-19 02:26:43 +0100

    Added close and minimize buttons for simple functionality

--- Freshly/MainWindow.xaml.cs
+++ Freshly/MainWindow.xaml.cs
@@ -33,5 +33,15 @@ namespace Freshly
                 this.DragMove();
             }
         }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
